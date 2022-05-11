021311371f8aec478c9e8e3c7c4c3f3eb2625d1c
Klay Clarke <clarkeklay@gmail.com>
2022-05-11 09:33:38 -0400

    add oauth buttons to the signup and login modals

--- components/Modal/Auth/AuthModal.tsx
+++ components/Modal/Auth/AuthModal.tsx
@@ -8,10 +8,14 @@ import {
   ModalCloseButton,
   ModalBody,
   ModalFooter,
+  Flex,
 } from "@chakra-ui/react";
 import React from "react";
 import { useRecoilState } from "recoil";
 import { AuthModalState } from "../../../atoms/authModalAtom";
+import Login from "./Login";
+import OAuthButtons from "./OAuthButtons";
+import Signup from "./Signup";
 
 const AuthModal: React.FC = () => {
   const [modalState, setModalState] = useRecoilState(AuthModalState);
@@ -26,9 +30,38 @@ const AuthModal: React.FC = () => {
       <Modal isOpen={modalState.open} onClose={handleClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader>
+            {modalState.view === "login"
+              ? "Log In"
+              : modalState.view === "signup"
+              ? "Sign Up"
+              : ""}
+          </ModalHeader>
           <ModalCloseButton />
-          <ModalBody>Modal body</ModalBody>
+          <ModalBody
+            display={"flex"}
+            flexDirection={"column"}
+            alignItems={"center"}
+            justifyContent={"center"}
+          >
+            <Flex
+              direction={"column"}
+              align={"center"}
+              justify={"center"}
+              width={"70%"}
+            >
+              <OAuthButtons />
+              <Flex direction={"column"} align={"center"} width={"100%"} mt={4}>
+                {modalState.view === "signup" ? (
+                  <Signup />
+                ) : modalState.view === "login" ? (
+                  <Login />
+                ) : (
+                  ""
+                )}
+              </Flex>
+            </Flex>
+          </ModalBody>
         </ModalContent>
       </Modal>
     </>
