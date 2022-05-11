6167e7c249262b616644d1d485b22b9b0c75f0ff
Klay Clarke <clarkeklay@gmail.com>
2022-05-11 08:05:55 -0400

    create a component to house log in and sign up buttons -- style said buttons

---
+++ components/Navbar/RightContent/AuthButtons.tsx
@@ -0,0 +1,34 @@
+import { Button } from "@chakra-ui/react";
+import React from "react";
+
+const AuthButtons: React.FC = () => {
+  return (
+    <>
+      <Button
+        variant={"outline"}
+        height="28px"
+        display={{ base: "none", sm: "flex" }}
+        width={{ base: "70px", md: "110px" }}
+        mr={2}
+        onClick={() => {
+          console.log("log in");
+        }}
+      >
+        Log In
+      </Button>
+      <Button
+        variant={"solid"}
+        height={"28px"}
+        display={{ base: "none", sm: "flex" }}
+        width={{ base: "70px", md: "110px" }}
+        mr={2}
+        onClick={() => {
+          console.log("sign up");
+        }}
+      >
+        Sign Up
+      </Button>
+    </>
+  );
+};
+export default AuthButtons;
