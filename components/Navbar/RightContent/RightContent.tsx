e263ccb56d3387d24ff536ff8846b207402e0729
Klay Clarke <clarkeklay@gmail.com>
2022-05-11 08:28:52 -0400

    add auth modal to the rightContent component

--- components/Navbar/RightContent/RightContent.tsx
+++ components/Navbar/RightContent/RightContent.tsx
@@ -1,5 +1,6 @@
 import { Flex } from "@chakra-ui/react";
 import React from "react";
+import AuthModal from "../../Modal/Auth/AuthModal";
 import AuthButtons from "./AuthButtons";
 
 type RightContentProps = {};
@@ -9,6 +10,7 @@ const RightContent: React.FC<RightContentProps> = () => {
     <>
       {/* <AuthModal/> */}
       <Flex justify={"center"} align={"center"}>
+        <AuthModal />
         <AuthButtons />
       </Flex>
     </>
