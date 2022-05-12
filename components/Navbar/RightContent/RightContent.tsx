60a3a75355a7befd42d6cbea760523081843acf4
Klay Clarke <clarkeklay@gmail.com>
2022-05-12 10:57:48 -0400

    if user signed in, show user first namein navbar, else show auth buttons

--- components/Navbar/RightContent/RightContent.tsx
+++ components/Navbar/RightContent/RightContent.tsx
@@ -1,17 +1,21 @@
 import { Flex } from "@chakra-ui/react";
+import { userInfo } from "os";
 import React from "react";
 import AuthModal from "../../Modal/Auth/AuthModal";
 import AuthButtons from "./AuthButtons";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../../firebase/clientApp";
 
 type RightContentProps = {};
 
 const RightContent: React.FC<RightContentProps> = () => {
+  const [user, loading, error] = useAuthState(auth);
   return (
     <>
       {/* <AuthModal/> */}
       <Flex justify={"center"} align={"center"}>
         <AuthModal />
-        <AuthButtons />
+        {user ? user.displayName?.toString().split(" ")[0] : <AuthButtons />}
       </Flex>
     </>
   );
