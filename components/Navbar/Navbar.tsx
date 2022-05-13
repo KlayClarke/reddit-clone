7343af026bbf66e0a6a7e8dd4c95c21bb13a89aa
Klay Clarke <clarkeklay@gmail.com>
2022-05-12 22:10:51 -0400

    edit nav styles -- if user authenticated, show them Directory dropdown (which houses the community creation modal)

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -2,21 +2,32 @@ import { Flex, Image } from "@chakra-ui/react";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase/clientApp";
+import Directory from "./Directory/Directory";
 import RightContent from "./RightContent/RightContent";
 import SearchInput from "./SearchInput";
 
 const Navbar: React.FC = () => {
   const [user, loading, error] = useAuthState(auth);
   return (
-    <Flex bg={"white"} height={"44px"} padding="6px 12px">
+    <Flex
+      bg={"white"}
+      height={"44px"}
+      padding="6px 12px"
+      justify={"space-between"}
+    >
       <Flex align={"center"}>
-        <Image src="/images/redditFace.svg" height={"30px"} />
+        <Image
+          src="/images/redditFace.svg"
+          height={"30px"}
+          mr={{ base: 2, md: 0 }}
+        />
         <Image
           src="/images/redditText.svg"
           height={"48px"}
           display={{ base: "none", md: "unset" }} // hide on small screens
         />
       </Flex>
+      {user && <Directory />}
       <SearchInput user={user} />
       <RightContent user={user} />
     </Flex>
