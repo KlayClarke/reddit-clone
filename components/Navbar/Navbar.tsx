3762dab92dda0aac911d0b710a2d2682e603f25a
Klay Clarke <clarkeklay@gmail.com>
2022-05-16 19:32:29 -0400

    make reddit logo in navbar redirect user to homepage ("/") on click -- cursor on pointer

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import { Flex, Image } from "@chakra-ui/react";
+import { useRouter } from "next/router";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase/clientApp";
@@ -8,6 +9,7 @@ import SearchInput from "./SearchInput";
 
 const Navbar: React.FC = () => {
   const [user, loading, error] = useAuthState(auth);
+  const router = useRouter();
   return (
     <Flex
       bg={"white"}
@@ -15,7 +17,13 @@ const Navbar: React.FC = () => {
       padding="6px 12px"
       justify={"space-between"}
     >
-      <Flex align={"center"}>
+      <Flex
+        align={"center"}
+        onClick={() => {
+          router.push("/");
+        }}
+        _hover={{ cursor: "pointer" }}
+      >
         <Image
           src="/images/redditFace.svg"
           height={"30px"}
