f6a87c2f39f3abca7fbdd22e68ea9234a90547b0
Klay Clarke <clarkeklay@gmail.com>
2022-05-16 10:48:43 -0400

    handle community creation -- validate community name, ensure it is unique, communicate with firestore to create document for new community

--- components/Modal/CreateCommunity/CreateCommunityModal.tsx
+++ components/Modal/CreateCommunity/CreateCommunityModal.tsx
@@ -16,9 +16,12 @@ import {
   Flex,
   Icon,
 } from "@chakra-ui/react";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
 import React, { useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { BsFillPersonFill, BsFillEyeFill } from "react-icons/bs";
 import { HiLockClosed } from "react-icons/hi";
+import { auth, firestore } from "../../../firebase/clientApp";
 
 type CreateCommunityModalProps = {
   open: boolean;
@@ -29,9 +32,12 @@ const CreateCommunityModal: React.FC<CreateCommunityModalProps> = ({
   open,
   handleClose,
 }) => {
+  const [user] = useAuthState(auth);
   const [communityName, setCommunityName] = useState("");
   const [charsRemaining, setCharsRemaining] = useState(21);
   const [communityType, setCommunityType] = useState("public");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.value.length > 21) return;
@@ -42,6 +48,49 @@ const CreateCommunityModal: React.FC<CreateCommunityModalProps> = ({
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
     setCommunityType(event.target.name);
+  };
+  const handleCreateCommunity = async () => {
+    // validate community name
+    const format = /[ `!@#$%^&*()+\-=\[\]{};':"\\|,.<>\/?~]/;
+    if (format.test(communityName)) {
+      setError(
+        "Community names can only contain letters, numbers and underscores"
+      );
+      return;
+    }
+
+    // set loading state to true while app is connecting to firestore database
+    setLoading(true);
+
+    try {
+      // check that name is unique -- if unique, create community
+      const communityDocumentReference = doc(
+        // get reference of document with same name in communities db
+        firestore,
+        "communities",
+        communityName
+      );
+      const communityDoc = await getDoc(communityDocumentReference); // find document based on reference / check if exists
+      if (communityDoc.exists()) {
+        // if community doc exists
+        throw new Error(
+          `The name r/${communityName} is already taken. Please try another!`
+        );
+      }
+      // create community (only runs if community name is unique and valid)
+      await setDoc(communityDocumentReference, {
+        creatorId: user?.uid,
+        createdAt: serverTimestamp(), // create timestamp of current time
+        numberOfMembers: 1,
+        privacyType: communityType,
+      });
+    } catch (error: any) {
+      console.log("handleCreateCommunity", error);
+      setError(error.message);
+    }
+
+    // set loading state to false after document creation is complete
+    setLoading(false);
   };
   return (
     <>
@@ -91,6 +140,9 @@ const CreateCommunityModal: React.FC<CreateCommunityModalProps> = ({
                 fontSize={"9pt"}
               >
                 {charsRemaining} characters remaining
+              </Text>
+              <Text fontSize={"9pt"} color={"red"} pt={1}>
+                {error}
               </Text>
               <Box mt={4} mb={4}>
                 <Text fontWeight={600} fontSize={15}>
@@ -146,7 +198,12 @@ const CreateCommunityModal: React.FC<CreateCommunityModalProps> = ({
             >
               Cancel
             </Button>
-            <Button variant={"solid"} height={"30px"}>
+            <Button
+              variant={"solid"}
+              height={"30px"}
+              onClick={handleCreateCommunity}
+              isLoading={loading}
+            >
               Create Community
             </Button>
           </ModalFooter>
