6eb1f7b06f220ce8f6761160e479eb6495e8933a
Klay Clarke <clarkeklay@gmail.com>
2022-05-20 13:12:50 -0400

    create MenuListItem component to house each community in the current user's Communities dropdown menu -- on click, the user is redirected to the home page of the clicked community

---
+++ components/Navbar/Directory/MenuListItem.tsx
@@ -0,0 +1,42 @@
+import { Flex, Icon, Image, MenuItem } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import React from "react";
+import { IconType } from "react-icons";
+
+type MenuListItemProps = {
+  displayText: string;
+  link: string;
+  icon: IconType;
+  iconColor: string;
+  imageURL?: string;
+};
+
+const MenuListItem: React.FC<MenuListItemProps> = ({
+  displayText,
+  link,
+  icon,
+  iconColor,
+  imageURL,
+}) => {
+  const router = useRouter();
+  return (
+    <MenuItem
+      width={"100%"}
+      fontSize={"10pt"}
+      _hover={{ bg: "gray.100" }}
+      onClick={() => {
+        router.push(link);
+      }}
+    >
+      <Flex align={"center"}>
+        {imageURL ? (
+          <Image src={imageURL} borderRadius={"full"} boxSize={"18px"} mr={2} />
+        ) : (
+          <Icon as={icon} fontSize={20} mr={2} color={iconColor} />
+        )}
+        {displayText}
+      </Flex>
+    </MenuItem>
+  );
+};
+export default MenuListItem;
