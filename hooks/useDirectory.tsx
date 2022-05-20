2160e8a4ecbc68c09e212772d477bd3ba3e61a7e
Klay Clarke <clarkeklay@gmail.com>
2022-05-20 14:50:15 -0400

    when user switches communities, change selectedMenuItem display to that of current community (incoming request)

--- hooks/useDirectory.tsx
+++ hooks/useDirectory.tsx
@@ -1,15 +1,21 @@
 import { useRouter } from "next/router";
-import React from "react";
-import { useRecoilState } from "recoil";
+import React, { useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { FaReddit } from "react-icons/fa";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { communityState } from "../atoms/communitiesAtom";
 import {
   DirectoryMenuItem,
   directoryMenuState,
 } from "../atoms/directoryMenuAtom";
+import { auth } from "../firebase/clientApp";
 
 const useDirectory = () => {
+  const router = useRouter();
+  const [user] = useAuthState(auth);
   const [directoryState, setDirectoryState] =
     useRecoilState(directoryMenuState);
-  const router = useRouter();
+  const communityStateValue = useRecoilValue(communityState);
   const onSelectMenuItem = (menuItem: DirectoryMenuItem) => {
     setDirectoryState((prev) => ({
       ...prev,
@@ -26,6 +32,25 @@ const useDirectory = () => {
       isOpen: !directoryState.isOpen,
     }));
   };
+
+  useEffect(() => {
+    const { currentCommunity } = communityStateValue;
+    if (currentCommunity) {
+      setDirectoryState((prev) => ({
+        ...prev,
+        selectedMenuItem: {
+          displayText: `r/${currentCommunity.id}`,
+          link: `/r/${currentCommunity.id}`,
+          imageURL: currentCommunity.imageURL,
+          icon: FaReddit,
+          iconColor:
+            user?.uid === currentCommunity.creatorId ? "brand.100" : "blue.500",
+        },
+      }));
+    }
+  }, [communityStateValue.currentCommunity]);
+
   return { directoryState, toggleMenuOpen, onSelectMenuItem };
 };
 export default useDirectory;
+  
