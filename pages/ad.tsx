8dea19d5c89accfb0280438a45ecf0e0e06de307
Klay Clarke <clarkeklay@gmail.com>
2022-05-22 12:49:53 -0400

    create ad page that will include a (1) form where user can draft an advert, and (2) a preview of said draft beside the form

---
+++ pages/ad.tsx
@@ -0,0 +1,54 @@
+import { Text } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { IoDocumentText, IoImagesOutline } from "react-icons/io5";
+import AdPreview from "../components/Ads/AdPreview";
+import NewAdPostForm, { TabItem } from "../components/Ads/NewAdForm";
+import PageContent from "../components/Layout/PageContent";
+import { auth } from "../firebase/clientApp";
+import usePosts from "../hooks/usePosts";
+import useSelectFile from "../hooks/useSelectFile";
+
+const formTabs: TabItem[] = [
+  {
+    title: "Post",
+    icon: IoDocumentText,
+  },
+  {
+    title: "Images & Video",
+    icon: IoImagesOutline,
+  },
+];
+
+type adProps = {};
+
+const ad: React.FC<adProps> = () => {
+  const [user] = useAuthState(auth);
+  const router = useRouter();
+  const { onVote, onSelectPost, onDeletePost } = usePosts();
+  const [selectedTab, setSelectedTab] = useState(formTabs[0].title);
+  const { selectedFile, setSelectedFile, onSelectFile } = useSelectFile();
+  const [adTitle, setAdTitle] = useState("");
+  return (
+    <PageContent>
+      <>
+        <NewAdPostForm
+          user={user!}
+          adTitle={adTitle}
+          setAdTitle={setAdTitle}
+          selectedFile={selectedFile}
+          setSelectedFile={setSelectedFile}
+          formTabs={formTabs}
+          selectedTab={selectedTab}
+          setSelectedTab={setSelectedTab}
+          onSelectFile={onSelectFile}
+        />
+      </>
+      <>
+        <AdPreview />
+      </>
+    </PageContent>
+  );
+};
+export default ad;
