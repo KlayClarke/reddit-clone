a18e9896b687abf13c45a9b53822f6be3d63eb08
Klay Clarke <clarkeklay@gmail.com>
2022-05-22 12:52:35 -0400

    create AdTextInputs component that holds textInput for AdPost

---
+++ components/Ads/AdForm/AdTextInputs.tsx
@@ -0,0 +1,55 @@
+import { Button, Flex, Input, Stack, Textarea } from "@chakra-ui/react";
+import { User } from "firebase/auth";
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../../firebase/clientApp";
+
+type AdTextInputsProps = {
+  adTitle: string;
+  onChange: (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
+  handleCreateAdPost: () => void;
+  loading: boolean;
+  user?: User;
+};
+
+const AdTextInputs: React.FC<AdTextInputsProps> = ({
+  adTitle,
+  onChange,
+  handleCreateAdPost,
+  loading,
+  user,
+}) => {
+  return (
+    <Stack spacing={3} width={"100%"}>
+      <Input
+        name="title"
+        fontSize={"10pt"}
+        borderRadius={4}
+        placeholder={"Title"}
+        _placeholder={{ color: "gray.500" }}
+        _focus={{
+          outline: "none",
+          bg: "white",
+          border: "1px solid",
+          borderColor: "black",
+        }}
+        value={adTitle}
+        onChange={onChange}
+      />
+      <Flex justify={"flex-end"}>
+        <Button
+          height={"34px"}
+          padding={"0px 30px"}
+          disabled={!adTitle}
+          isLoading={loading}
+          onClick={handleCreateAdPost}
+        >
+          Post
+        </Button>
+      </Flex>
+    </Stack>
+  );
+};
+export default AdTextInputs;
