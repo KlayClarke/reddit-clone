fe34748d73490f39393dca60c450c24b897739b3
Klay Clarke <clarkeklay@gmail.com>
2022-05-23 00:34:12 -0400

    randomize Chevron icons on Recommendations list items

--- components/Community/Recommendations.tsx
+++ components/Community/Recommendations.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Flex,
   Icon,
@@ -11,8 +12,10 @@ import {
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { BsDashLg } from "react-icons/bs";
 import { FaReddit } from "react-icons/fa";
-import { HiOutlineChevronUp } from "react-icons/hi";
+import { HiOutlineChevronDown, HiOutlineChevronUp } from "react-icons/hi";
+import { VscDash } from "react-icons/vsc";
 import { Community, communityState } from "../../atoms/communitiesAtom";
 import { auth } from "../../firebase/clientApp";
 import useCommunityData from "../../hooks/useCommunityData";
@@ -28,6 +31,9 @@ const Recommendations: React.FC = () => {
     setTimeout(() => {
       setLoadingFetch(false);
     }, 3000);
+  };
+  const getRandomInt = (max: number) => {
+    return Math.floor(Math.random() * max);
   };
   useEffect(() => {
     getCommunityRecommendations();
@@ -92,12 +98,47 @@ const Recommendations: React.FC = () => {
                       <Text fontWeight={700}>{index + 1}</Text>
                     </Flex>
                     <Flex align={"center"} width={"90%"}>
-                      <Icon
-                        as={HiOutlineChevronUp}
-                        fontSize={17}
-                        mr={2}
-                        color={"green.400"}
-                      />
+                      {getRandomInt(3) === 2 &&
+                      index !== topCommunities.length - 1 ? (
+                        <>
+                          <Icon
+                            as={HiOutlineChevronUp}
+                            fontSize={17}
+                            mr={2}
+                            color={"green.400"}
+                          />
+                        </>
+                      ) : getRandomInt(3) === 1 && index > 0 ? (
+                        <>
+                          <Icon
+                            as={HiOutlineChevronDown}
+                            fontSize={17}
+                            mr={2}
+                            color={"red.400"}
+                          />
+                        </>
+                      ) : getRandomInt(3) === 0 ? (
+                        <>
+                          <Icon
+                            as={VscDash}
+                            fontSize={17}
+                            mr={2}
+                            color={"black"}
+                            visibility={"hidden"}
+                          />
+                        </>
+                      ) : (
+                        <>
+                          <Icon
+                            as={VscDash}
+                            fontSize={17}
+                            mr={2}
+                            color={"black"}
+                            visibility={"hidden"}
+                          />
+                        </>
+                      )}
+
                       {community.imageURL ? (
                         <Image
                           src={community.imageURL}
